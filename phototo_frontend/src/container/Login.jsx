70c038fc4d357875352de84aa776f9fc375c8982
6210743073 <poohmiphan@outlook.com>
2022-02-14 14:54:22 +0700

    'update'

--- phototo_frontend/src/container/Login.jsx
+++ phototo_frontend/src/container/Login.jsx
@@ -1,103 +1,64 @@
-import React from "react";
-import GoogleLogin from "react-google-login";
-import { useNavigate } from "react-router-dom";
-import { FcGoogle } from "react-icons/fc";
-import { client } from "../client";
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/LightLogo.png';
+
+import { client } from '../client';
+
 const Login = () => {
   const navigate = useNavigate();
   const responseGoogle = (response) => {
-    localStorage.setItem("user", JSON.stringify(response.profileObj));
-    const { name, googleId, email, imageUrl } = response.profileObj;
+    localStorage.setItem('user', JSON.stringify(response.profileObj));
+    const { name, googleId, imageUrl } = response.profileObj;
     const doc = {
       _id: googleId,
-      _type: "user",
+      _type: 'user',
       userName: name,
-      email: email,
       image: imageUrl,
     };
     client.createIfNotExists(doc).then(() => {
-      navigate("/", { replace: true });
+      navigate('/', { replace: true });
     });
   };
-  return (
-    <div className="flex flex-col md:flex-row-reverse h-screen items-center">
-      <div className="hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-        <img
-          alt="ภาพพื้นหลัง"
-          className="w-full h-full object-cover "
-          src="https://images.unsplash.com/photo-1643198964380-a3d9a8b47f68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-        />
-      </div>
-      <div
-        className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto  md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
-        flex items-center justify-center"
-      >
-        <div className="w-full h-100">
-          <h1 className="text-5xl text-center font-bold">PhoToTo</h1>
-          <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">
-            Login to your account
-          </h1>
-          <form className="mt-6">
-            <div>
-              <label className="block text-gray-700">Email Address</label>
-              <input
-                type="email"
-                placeholder="Enter Email Address"
-                className="w-full bg-gray-200 rounded-lg  mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none px-4 py-3"
-                required
-              />
-            </div>
-            <div className="mt-4 ">
-              <label className="block text-gray-700">Password</label>
-              <input
-                type="password"
-                placeholder="Enter Password"
-                minLength={6}
-                name=""
-                className="w-full bg-gray-200 rounded-lg  mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none px-4 py-3"
-                required
-              />
-            </div>
 
-            <button
-              className="w-full bg-blue-500 hover:bg-blue-400 px-4 py-3 mt-6 rounded-lg font-semibold text-white focus:bg-blue-400 focus:outline-none"
-              type="sunmit"
-            >
-              Log in
-            </button>
-          </form>
-          <hr className="my-6 border-gray-300 w-full" />
-          <GoogleLogin
-            clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
-            render={(renderProps) => (
-              <button
-                type="button"
-                className="w-full block bg-white border-gray-300 hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semiblod rounded-lg px-4 py-3 border"
-                onClick={renderProps.onClick}
-                disabled={renderProps.disabled}
-              >
-                <div className="flex items-center justify-center ">
-                  <FcGoogle className="" aria-hidden="true" />
-                  <span className="ml-4">Sign in with Google</span>
-                </div>
-              </button>
-            )}
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy="single_host_origin"
-          />
-          <p className="mt-8 ">
-            Need an account?
-            <a
-              href="/register"
-              className="text-blue-500 hover:text-blue-700 font-semiblod"
-            >
-              create an account
-            </a>
-          </p>
-          <p className="text-sm text-center text-gray-500 mt-12">
-            &copy; 2022 PhoToTo Login Landing Page
-          </p>
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className=" relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" />
+          </div>
+
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
+          </div>
         </div>
       </div>
     </div>
