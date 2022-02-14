f8f9c756f6b51000cc56773081788b66d2e00412
6210743073 <poohmiphan@outlook.com>
2022-02-14 15:04:34 +0700

    'fixapp'

--- phototo_frontend/src/App.js
+++ phototo_frontend/src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import Login from "./container/Login"
-import Register from "./container/Register"
+
+import { Login } from './components';
 import Home from './container/Home';
 
 const App = () => {
-    return (<Routes>
-        <Route path='login' element={<Login />} />
-        <Route path='register' element={<Register />} />
-        <Route path='/*' element={<Home />} />
-    </Routes>
-    )
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+        if (!User) navigate('/login');
+    }, []);
+
+    return (
+        <Routes>
+            <Route path="login" element={<Login />} />
+            <Route path="/*" element={<Home />} />
+        </Routes>
+    );
 };
 
 export default App;
